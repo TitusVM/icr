2468f83e1e6c8512ee90ecfb68826ce2915e47fb
Titus Abele <tvmab@pm.me>
2024-06-05 18:06:37 +0200

    complete report

---
+++ 06-mp/listings/create_procedure.sh
@@ -0,0 +1,9 @@
+-------------------------------------------------------------
+Welcome to SafeStore, a secure file storage system
+-------------------------------------------------------------
+
+[DEBUG] Creating Alice and Bob's accounts...
+[DEBUG] Alice and Bob's accounts have been created
+User ID: 1b851e5a-ef28-4c9f-8e54-a56aefa45a7c, Name: Alice
+User ID: 0ab379fe-f8dd-4802-97c9-f2436f419d30, Name: Bob
+[DEBUG] Alice and Bob's root folders have been created
